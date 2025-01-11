512acf7d3035e0a82ad169c0bb74a172790a4720
fbuchmann <anmeldungen@buchmann.digital>
2025-01-11 15:53:44 +0100

    add the 404-not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,19 @@
+import { Footer } from '@/components/Footer';
+import { WebsiteAnalysis } from '@/components/WebsiteAnalysis';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow">
+        <div className="container mx-auto px-4 py-16 text-center">
+          <Link href="/">
+            <span className="text-indigo-600 hover:text-indigo-800">Zur zur Startseite</span>
+          </Link>
+        </div>
+        <WebsiteAnalysis />
+      </main>
+      <Footer />
+    </div>
+  );
+}
