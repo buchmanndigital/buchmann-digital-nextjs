f0937044ce39491ded38da34c2070fb1837850ac
buchmanndigital <anmeldungen@buchmann.digital>
2025-04-01 22:46:33 +0200

    facebook pixel anpassungen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 import { useEffect, useState } from "react";
 import Head from "next/head";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -68,6 +69,32 @@ export default function RootLayout({
       <head>
         <title>Websites und Software die Unternehmen voranbringen</title>
         <meta name="description" content="Professionelle Webentwicklung und Softwarelösungen für Ihr Unternehmen" />
+        {consent && (
+          <>
+            <Script id="facebook-pixel">
+              {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
+                fbq('track', 'PageView');
+              `}
+            </Script>
+            <noscript>
+              <img 
+                height="1" 
+                width="1" 
+                style={{ display: 'none' }}
+                src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
+              />
+            </noscript>
+          </>
+        )}
         {typeof window !== 'undefined' && window.location.pathname === '/digitalisierung/danke' && (
           <script
             dangerouslySetInnerHTML={{
